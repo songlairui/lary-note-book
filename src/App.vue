0dc9ff547d347d6464aa90a86f28784914bf41eb
songlairui <cnre@live.cn>
2019-05-07 02:12:14 +0800

    添加登陆

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,32 @@
 <template>
   <a-locale-provider :locale="zh_CN">
     <div id="app">
-      <img src="./assets/logo.png">
       <div>
-        <p>
-          If Ant-Design-Vue is successfully added to this project, you'll see an
-          <code v-text="'<a-button>'"></code>
-          <code v-text="'<a-pagination>'"></code>
-          below
-        </p>
-        <a-button type="primary">Primary</a-button>
-        <a-pagination size="small" :total="50" showSizeChanger showQuickJumper />
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/login">Login</router-link>
       </div>
-      <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <router-view></router-view>
     </div>
   </a-locale-provider>
 </template>
 
 <script>
-import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN'
-import HelloWorld from './components/HelloWorld.vue'
+import zh_CN from "ant-design-vue/lib/locale-provider/zh_CN";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   data() {
     return {
-      zh_CN,
-    }
+      zh_CN
+    };
   },
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
