5514e062135b31f028f5a69b09b467bba4337c41
songlairui <cnre@live.cn>
2019-05-08 03:11:23 +0800

    更改样式

--- src/views/NoteList.vue
+++ src/views/NoteList.vue
@@ -1,53 +1,62 @@
 <template>
   <div class="note-list">
-    <ApolloQuery
-      :query="require('../graphql/my-notes.gql')"
-      :variables="{ first, skip }"
-      @result="handleResult"
-    >
-      <template slot-scope="{ result: { loading, error, data } }">
-        <!-- Loading -->
-        <div v-if="loading" class="loading apollo">Loading...</div>
-
-        <!-- Error -->
-        <div v-else-if="error" class="error apollo">An error occured</div>
-
-        <!-- Result -->
-        <div v-else-if="data" class="result apollo">
-          <a-list :grid="{ gutter: 16, column: 4 }" :dataSource="data.notes">
-            <a-list-item slot="renderItem" slot-scope="item, index">
-              <a-card :title="item.title">{{item.content}}</a-card>
-            </a-list-item>
-          </a-list>
-        </div>
-
-        <!-- No result -->
-        <div v-else class="no-result apollo">No result :(</div>
-      </template>
-    </ApolloQuery>
-    <a-pagination v-model="pageNumber"/>
+    <div class="card-blocks">
+      <div class="card-wrapper" v-for="item in source.edges">
+        <a-card :title="item.node.title">{{item.node.content}}</a-card>
+      </div>
+    </div>
+    <div class="actions">
+      <button v-if="source.hasNextPage" @click="showMore">Show more</button>
+    </div>
   </div>
 </template>
 
 <script>
+import MY_NOTES from "../graphql/my-notes.gql";
+
+const initVari = {
+  first: 5,
+  skip: 0
+};
+
 export default {
   name: "NoteList",
   data() {
     return {
-      noteList: [],
       first: 5,
-      skip: 0,
-      pageTotal: 0
+      skip: 0
     };
   },
+  apollo: {
+    notesConnection: {
+      query: MY_NOTES,
+      variables: initVari
+    },
+    notesStatistics: {
+      query: MY_NOTES,
+      variables: initVari
+    }
+  },
   computed: {
-    pageNumber: {
-      get() {
-        return Math.floor(this.skip / this.first) + 1;
-      },
-      set(val) {
-        this.skip = this.first * (val - 1);
-      }
+    variables() {
+      return {
+        first: this.first,
+        skip: this.skip
+      };
+    },
+    source() {
+      const { notesConnection, notesStatistics } = this;
+      if (!notesConnection)
+        return {
+          total: 0,
+          hasNextPage: false,
+          edges: []
+        };
+      return {
+        total: notesStatistics.aggregate.count,
+        hasNextPage: notesConnection.pageInfo.hasNextPage,
+        edges: notesConnection.edges
+      };
     }
   },
   methods: {
@@ -61,16 +70,47 @@ export default {
         });
         return;
       }
+    },
+    async loadData() {
+      const { data } = await this.$apollo.query({
+        query: MY_NOTES,
+        variables: this.variables
+      });
+      if (!data || !data.notesConnection) {
+        this.$message.error("No Data!");
+        return;
+      }
       const {
-        notesConnection: {
-          aggregate: { count }
-        }
+        notesConnection: { aggregate, edges, pageInfo }
       } = data;
-      this.pageTotal = count;
+      this.noteList.push(...edges);
+      this.showMoreEnabled = pageInfo.hasNextPage;
+    },
+    showMore() {
+      this.skip += this.first;
+      this.$apollo.queries.notesConnection.fetchMore({
+        variables: this.variables,
+        updateQuery: (prevResult, { fetchMoreResult }) => {
+          fetchMoreResult.notesConnection.edges.unshift(
+            ...prevResult.notesConnection.edges
+          );
+          return fetchMoreResult;
+        }
+      });
     }
+  },
+  mounted() {
+    // this.loadData();
   }
 };
 </script>
-
-<style>
+<style lang="less" scoped>
+.card-blocks {
+  display: flex;
+  flex-wrap: wrap;
+  .card-wrapper {
+    padding: 1em;
+    flex: 0 0 23em;
+  }
+}
 </style>
