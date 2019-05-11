98eca97b13b11a2c2ceb855eb59aba9a43d44e99
songlairui <cnre@live.cn>
2019-05-11 21:03:27 +0800

    add gzip

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const CompressionPlugin = require('compression-webpack-plugin')
+
 module.exports = {
   lintOnSave: false,
 
@@ -20,9 +22,23 @@ module.exports = {
       }
     }
   },
-  chainWebpack: () => {
+  chainWebpack: (config) => {
     console.group()
     console.info('VUE_APP_GRAPHQL_HTTP:', process.env.VUE_APP_GRAPHQL_HTTP)
+    if (process.env.NODE_ENV === 'production') {
+      console.info('use gzip')
+      config
+        .plugin('compression')
+        .use(CompressionPlugin, {
+          asset: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'),
+          threshold: 10240,
+          minRatio: 0.8,
+          cache: true
+        })
+        .tap(() => {})
+    }
     console.groupEnd()
   }
 }
