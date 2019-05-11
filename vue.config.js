0f6cba220eb1478aa247ba1b1e8c2103f98ca2a4
songlairui <cnre@live.cn>
2019-05-11 21:34:21 +0800

    opti

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,39 @@ module.exports = {
   chainWebpack: (config) => {
     console.group()
     console.info('\nVUE_APP_GRAPHQL_HTTP:', process.env.VUE_APP_GRAPHQL_HTTP)
+    var externals = {
+      vue: 'Vue',
+      axios: 'axios',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      moment: 'moment'
+    }
+    const cdn = {
+      css: [
+        // element-ui css
+        // '//unpkg.com/element-ui/lib/theme-chalk/index.css'
+      ],
+      js: [
+        // vue
+        '//cdn.jsdelivr.net/npm/vue',
+        // vue-router
+        '//cdn.jsdelivr.net/npm/vue-router',
+        // vuex
+        '//cdn.jsdelivr.net/npm/vuex',
+        // axios
+        '//cdn.jsdelivr.net/npm/axios',
+        '//cdn.jsdelivr.net/npm/moment'
+      ]
+    }
+
     if (process.env.NODE_ENV === 'production') {
+      console.info('use externals')
+      config.externals(externals)
+
+      config.plugin('html').tap((args) => {
+        args[0].cdn = cdn
+        return args
+      })
       console.info('use gzip')
       config
         .plugin('compression')
